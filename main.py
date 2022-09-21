c28b5d3d3968b6112b3eb219016a7bf673fea15e
Angel Shau <shauangel2000@gmail.com>
2022-09-21 12:25:37 +0800

    Update main.py
    add login manager

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
+##flask basic framework
 from flask import Flask
-
-from flask_cors import CORS
 from views import register_blueprint
 
+#flask-cors: calling api from web
+from flask_cors import CORS
+
+#flask-login: sign in authentication
+from models.PSAbotLoginManager import PSAbotLoginManager,UserModel
+
+#middleware proxy fix: make nginx reverse proxy point to the app directly
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+
+#set flask app
 app = Flask(__name__)
+
+#reload when templates changes
 app.jinja_env.auto_reload = True
+
+#call api through web
+CORS(app)
+
+# --------- login --------- #
+login_manager = PSAbotLoginManager(app)
+@login_manager.user_loader
+def user_loader(user_id):  
+    user_now = UserModel(user_id)   
+    return user_now
+# ------------------------- #
+
+#regist app
 register_blueprint(app)
+
+
 app.wsgi_app = ProxyFix(app.wsgi_app)
+
